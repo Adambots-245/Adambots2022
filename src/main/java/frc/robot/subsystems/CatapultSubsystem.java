98de3df8ffbf722366c94d801e377011924c4290
Porter Youngman <porteryoungman@gmail.com>
2022-02-09 19:33:13 -0500

    I added imports for montor control

--- src/main/java/frc/robot/subsystems/CatapultSubsystem.java
+++ src/main/java/frc/robot/subsystems/CatapultSubsystem.java
@@ -7,6 +7,9 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CatapultSubsystem extends SubsystemBase {
