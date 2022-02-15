c70730329746134760ea8d61f99e2a41050b0730
Aarush Gota <aarush.gota@gmail.com>
2022-02-14 21:24:29 -0500

    started to implement blue and red ball detection

--- src/main/java/frc/robot/subsystems/VisionProcessorSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionProcessorSubsystem.java
@@ -29,18 +29,33 @@ public class VisionProcessorSubsystem extends SubsystemBase {
     private static RedGripPipeline redGrip;
     private static BlueGripPipeline blueGrip;
     private static Mat mat;
-    private static Point crosshair;
-    private static Point[] pts = new Point[4];
+    private static Point hubCrosshair;
+    private static Point blueCrosshair;
+    private static Point redCrosshair;
+    private static Point[] hubPts = new Point[4];
+    private static Point[] bluePts = new Point[4];
+    private static Point[] redPts = new Point[4];
     private static int pixelDistance;
-    private static double angle;
+    private static double hubAngle;
+    private static double blueAngle;
+    private static double redAngle;
     private Object lock = new Object();
     private Thread visionThread;
     private NetworkTableEntry angleEntry;
     private Solenoid ringLight;
-    private MedianFilter maxYFilter;
-    private MedianFilter minYFilter;
-    private MedianFilter maxXFilter;
-    private MedianFilter minXFilter;
+    private MedianFilter hubMaxYFilter;
+    private MedianFilter hubMinYFilter;
+    private MedianFilter hubMaxXFilter;
+    private MedianFilter hubMinXFilter;
+    private MedianFilter redMaxYFilter;
+    private MedianFilter redMinYFilter;
+    private MedianFilter redMaxXFilter;
+    private MedianFilter redMinXFilter;
+    private MedianFilter blueMaxYFilter;
+    private MedianFilter blueMinYFilter;
+    private MedianFilter blueMaxXFilter;
+    private MedianFilter blueMinXFilter;
+    //int finalAngle;
 
     public VisionProcessorSubsystem(Solenoid ringLight, RedGripPipeline redGrip, HubGripPipeline hubGrip, BlueGripPipeline blueGrip) {
         this.ringLight = ringLight;
@@ -55,11 +70,23 @@ public class VisionProcessorSubsystem extends SubsystemBase {
         // ringLight.clearAllPCMStickyFaults();
         // if pcm status is flashing orange (sticky fault), run once
         // ringLight = new Solenoid(Constants.RING_LIGHT_PORT);
-        // Creates a MedianFilter with a window size of 5 samples
-        maxYFilter = new MedianFilter(5);
-        minYFilter = new MedianFilter(5);
-        maxXFilter = new MedianFilter(5);
-        minXFilter = new MedianFilter(5);
+        // Creates a MedianFilter with a window size of 5 samples for hub
+        hubMaxYFilter = new MedianFilter(5);
+        hubMinYFilter = new MedianFilter(5);
+        hubMaxXFilter = new MedianFilter(5);
+        hubMinXFilter = new MedianFilter(5);
+
+        //red
+        redMaxYFilter = new MedianFilter(5);
+        redMinYFilter = new MedianFilter(5);
+        redMaxXFilter = new MedianFilter(5);
+        redMinXFilter = new MedianFilter(5);
+
+        //blue
+        blueMaxYFilter = new MedianFilter(5);
+        blueMinYFilter = new MedianFilter(5);
+        blueMaxXFilter = new MedianFilter(5);
+        blueMinXFilter = new MedianFilter(5);
  
         ringLight.set(true);
         ballDetectionCamera = CameraServer.startAutomaticCapture(Constants.BALL_CAM_NUMBER);
@@ -104,16 +131,17 @@ public class VisionProcessorSubsystem extends SubsystemBase {
         // Main vision loop
         int frameCount = 0;
         while (!Thread.interrupted()) {
-            crosshair = null;
+            hubCrosshair = null;
             if (cvSink.grabFrame(mat) == 0) {
+                processedOutputStreamHub.notifyError(cvSink.getError());
                 processedOutputStreamRed.notifyError(cvSink.getError());
+                processedOutputStreamBlue.notifyError(cvSink.getError());
                 continue;
 
             }
-
             hubGrip.process(mat);
-            //redGrip.process(mat);
-            //blueGrip.process(mat);
+            redGrip.process(mat);
+            blueGrip.process(mat);
 
         /*    RotatedRect[] rects = findBoundingBoxes();
             if (rects.length != 0) {
@@ -133,22 +161,20 @@ public class VisionProcessorSubsystem extends SubsystemBase {
               }
 
                */
-            RotatedRect[] rects = findBoundingBoxesHub();
-            
+            findBoundingBoxesHub();
+            findBoundingBoxesBlue();
+            findBoundingBoxesRed();
 
-/*
-            if (crosshair != null) {
+            if (hubCrosshair != null) {
                 synchronized (lock) {
-                    calculateAngle();
-
+                    calculateAngle(hubAngle, hubCrosshair);
                 }
-                
             }
-*/            
+          
             if (frameCount == 1) {
                 processedOutputStreamHub.putFrame(mat);
-                //processedOutputStreamRed.putFrame(redGrip.hsvThresholdOutput());
-                //processedOutputStreamBlue.putFrame(blueGrip.hsvThresholdOutput());
+                processedOutputStreamRed.putFrame(mat);
+                processedOutputStreamBlue.putFrame(mat);
                 frameCount = 0;
             }
 
@@ -157,104 +183,172 @@ public class VisionProcessorSubsystem extends SubsystemBase {
 
     }
 
-    public RotatedRect[] findBoundingBoxesHub() {
+    public void findBoundingBoxesRed() {
+        ArrayList<MatOfPoint> contours = redGrip.filterContoursOutput();
+        RotatedRect[] rects = new RotatedRect[contours.size()];
+        for (int i = 0; i < contours.size(); i++){
+            rects[i] = Imgproc.minAreaRect(new MatOfPoint2f(contours.get(i).toArray()));
+        }
+
+        if (contours.size() != 0) {
+            double rMinX = rects[0].boundingRect().x;
+            double rMaxX = 0;
+            double rMinY = rects[0].boundingRect().y;
+            double rMaxY = 0;
+
+            for (int a = 0; a < rects.length; a++) {
+                rMinX = Math.min(rMinX, rects[a].boundingRect().x);
+                rMaxX = Math.max(rMaxX, rects[a].boundingRect().x);
+                rMinY = Math.min(rMinY, rects[a].boundingRect().y);
+                rMaxY = Math.max(rMaxY, rects[a].boundingRect().y);
+            }
+
+            double frMinX = redMinXFilter.calculate(rMinX);
+            double frMaxX = redMaxXFilter.calculate(rMaxX);
+            double frMinY = redMinYFilter.calculate(rMinY);
+            double frMaxY = redMaxYFilter.calculate(rMaxY);
+
+            redPts[0] = new Point(frMinX, frMinY);
+            redPts[1] = new Point(frMaxX, frMinY);
+            redPts[2] = new Point(frMaxX, frMaxY);
+            redPts[3] = new Point(frMinX, frMaxY);
+
+            drawRect(redPts);
+            findCrosshair(redPts);
+        
+            if (redCrosshair != null){
+                drawCrosshair(redCrosshair);
+                calculateAngle(redAngle, redCrosshair);
+            }
+            SmartDashboard.putNumber("Angle", hubAngle);
+        }
+    }
+
+    public void findBoundingBoxesBlue() {
+        ArrayList<MatOfPoint> contours = blueGrip.filterContoursOutput();
+        RotatedRect[] rects = new RotatedRect[contours.size()];
+        for (int i = 0; i < contours.size(); i++){
+            rects[i] = Imgproc.minAreaRect(new MatOfPoint2f(contours.get(i).toArray()));
+        }
+
+        if (contours.size() != 0) {
+            double bMinX = rects[0].boundingRect().x;
+            double bMaxX = 0;
+            double bMinY = rects[0].boundingRect().y;
+            double bMaxY = 0;
+
+            for (int a = 0; a <rects.length; a++) {
+                bMinX = Math.min(bMinX, rects[a].boundingRect().x);
+                bMaxX = Math.max(bMaxX, rects[a].boundingRect().x);
+                bMinY = Math.min(bMinY, rects[a].boundingRect().y);
+                bMaxY = Math.max(bMaxY, rects[a].boundingRect().y);
+            }
+
+            double fbMinX = blueMinXFilter.calculate(bMinX);
+            double fbMaxX = blueMaxXFilter.calculate(bMaxX);
+            double fbMinY = blueMinYFilter.calculate(bMinY);
+            double fbMaxY = blueMaxYFilter.calculate(bMaxY);
+
+            bluePts[0] = new Point(fbMinX, fbMinY);
+            bluePts[1] = new Point(fbMaxX, fbMinY);
+            bluePts[2] = new Point(fbMaxX, fbMaxY);
+            bluePts[3] = new Point(fbMinX, fbMaxY);
+
+            drawRect(bluePts);
+            findCrosshair(bluePts);
+        
+            if (blueCrosshair != null){
+                drawCrosshair(blueCrosshair);
+                calculateAngle(blueAngle, blueCrosshair);
+            }
+            SmartDashboard.putNumber("Angle", hubAngle);
+        }
+    }
+
+    public void findBoundingBoxesHub() {
         ArrayList<MatOfPoint> contours = hubGrip.filterContoursOutput();
         //System.out.println(contours.size());
         RotatedRect[] rects = new RotatedRect[contours.size()];
         for (int i = 0; i < contours.size(); i++)
             rects[i] = Imgproc.minAreaRect(new MatOfPoint2f(contours.get(i).toArray()));
 
-        /*System.out.print(rects.length);
-        SmartDashboard.putNumber("rect0 x", rects[0].boundingRect().x);
-        SmartDashboard.putNumber("rect last x-1", rects[rects.length - 1].boundingRect().x);
-        SmartDashboard.putNumber("rect0 y", rects[0].boundingRect().y);
-        SmartDashboard.putNumber("rect last y-1", rects[rects.length - 1].boundingRect().y);
-*/
         if (contours.size() != 0){
-            double minX = rects[0].boundingRect().x;
-        double maxX = 0;
-        double minY = rects[0].boundingRect().y;
-        double maxY = 0;
+            double hMinX = rects[0].boundingRect().x;
+            double hMaxX = 0;
+            double hMinY = rects[0].boundingRect().y;
+            double hMaxY = 0;
 
-        for(int a=0; a<rects.length; a++ ) {
-            minX = Math.min(minX, rects[a].boundingRect().x);
-            maxX = Math.max(maxX, rects[a].boundingRect().x);
-            minY = Math.min(minY, rects[a].boundingRect().y);
-            maxY = Math.max(maxY, rects[a].boundingRect().y);
+            for(int a=0; a<rects.length; a++ ) {
+                hMinX = Math.min(hMinX, rects[a].boundingRect().x);
+                hMaxX = Math.max(hMaxX, rects[a].boundingRect().x);
+                hMinY = Math.min(hMinY, rects[a].boundingRect().y);
+                hMaxY = Math.max(hMaxY, rects[a].boundingRect().y);
+            }
 
-
-        }
-
-        double fMinY = minYFilter.calculate(minY);
-        double fMaxY = maxYFilter.calculate(maxY);
-        double fMinX = minXFilter.calculate(minX);
-        double fMaxX = maxXFilter.calculate(maxX);
+            double fhMinY = hubMinYFilter.calculate(hMinY);
+            double fhMaxY = hubMaxYFilter.calculate(hMaxY);
+            double fhMinX = hubMinXFilter.calculate(hMinX);
+            double fhMaxX = hubMaxXFilter.calculate(hMaxX);
         
-        SmartDashboard.putNumber("minX", fMinY);
-        SmartDashboard.putNumber("minY", fMaxY);
-        SmartDashboard.putNumber("maxX", fMinX);
-        SmartDashboard.putNumber("maxY", fMaxX);
+            //SmartDashboard.putNumber("HubMinX", fMinY);
+            //SmartDashboard.putNumber("HubMinY", fMaxY);
+            //SmartDashboard.putNumber("HubMaxX", fMinX);
+            //SmartDashboard.putNumber("HubMaxY", fMaxX);
+
+            //RotatedRect boundingBox = new RotatedRect();
+            hubPts[0] = new Point(fhMinX, fhMinY);
+            hubPts[1] = new Point(fhMaxX, fhMinY);
+            hubPts[2] = new Point(fhMaxX, fhMaxY);
+            hubPts[3] = new Point(fhMinX, fhMaxY);
+
+            double fieldOfView = 68.5;
+            int pixelWidth = (int) (fhMaxX - fhMinX);
+            int initialDistance = 96;
+            int calculatedDistance = 0; 
+            int error = 8;
+            double slope = 0.125;
         
+            double width = 1016; // width of the hub in mm
+            //focalLength = (pixels * initialDistance) / width;
+            double radVal = Math.toRadians(fieldOfView);
+            double arcTanVal = Constants.IMG_HEIGHT / Constants.IMG_WIDTH;
+            double cosVal = Math.cos(arcTanVal);
+            double tanVal = Math.tan(radVal * cosVal);
+            double angrad = Math.atan(tanVal);
+            double horizontalFieldOfView = Math.toDegrees(angrad);
+            // H_FOV = np.degrees(np.arctan(np.tan(np.radians(D_FOV)*np.cos(np.arctan(height/width)))))
+            double focalLength = Constants.IMG_WIDTH / (2*Math.tan(Math.toRadians(horizontalFieldOfView/2)));
+            //focalLength = 60; // mm https://commons.wikimedia.org/wiki/File:Microsoft_Lifecam_HD-3000_webcam.jpg
+            calculatedDistance = (int) (((width * focalLength) / pixelWidth)/25.4); // in inch
+
+            // equation for accurate distance to hub
+            //double finalError = error + slope * (calculatedDistance - initialDistance);
+            // =8+0.125*(A10-96)
+            double a =0.00199916;
+            double b = -0.450253;
+            double c = 35.7879;
+
+            double parabolaError = a * Math.pow(calculatedDistance, 2) + b * calculatedDistance + c;
+            //0.00199916x^{2}-0.450253x+35.7879
+
+            int finalDistance = (int) (calculatedDistance + parabolaError);
         
+            SmartDashboard.putNumber("initial distance", calculatedDistance);
+            SmartDashboard.putNumber("final distance", finalDistance);
+            SmartDashboard.putNumber("focalLength", focalLength);
+            SmartDashboard.putNumber("pixelWidth", pixelWidth);
+            SmartDashboard.putNumber("parabola Error", parabolaError);
 
-        //RotatedRect boundingBox = new RotatedRect();
-     
-        pts[0] = new Point(fMinX, fMinY);
-        pts[1] = new Point(fMaxX, fMinY);
-        pts[2] = new Point(fMaxX, fMaxY);
-        pts[3] = new Point(fMinX, fMaxY);
-
-
-        double fieldOfView = 68.5;
-        int pixelWidth = (int) (fMaxX - fMinX);
-        int initialDistance = 96;
-        int calculatedDistance = 0; 
-        int error = 8;
-        double slope = 0.125;
-
+            //if ()
+            drawRect(hubPts);
+            findCrosshair(hubPts);
         
-        double width = 1016; // width of the hub in mm
-        //focalLength = (pixels * initialDistance) / width;
-        double radVal = Math.toRadians(fieldOfView);
-        double arcTanVal = Constants.IMG_HEIGHT / Constants.IMG_WIDTH;
-        double cosVal = Math.cos(arcTanVal);
-        double tanVal = Math.tan(radVal * cosVal);
-        double angrad = Math.atan(tanVal);
-        double horizontalFieldOfView = Math.toDegrees(angrad);
-        // H_FOV = np.degrees(np.arctan(np.tan(np.radians(D_FOV)*np.cos(np.arctan(height/width)))))
-        double focalLength = Constants.IMG_WIDTH / (2*Math.tan(Math.toRadians(horizontalFieldOfView/2)));
-        //focalLength = 60; // mm https://commons.wikimedia.org/wiki/File:Microsoft_Lifecam_HD-3000_webcam.jpg
-        calculatedDistance = (int) (((width * focalLength) / pixelWidth)/25.4); // in inch
-
-        // equation for accurate distance to hub
-        //double finalError = error + slope * (calculatedDistance - initialDistance);
-        // =8+0.125*(A10-96)
-        double a =0.00199916;
-        double b = -0.450253;
-        double c = 35.7879;
-
-        double parabolaError = a * Math.pow(calculatedDistance, 2) + b * calculatedDistance + c;
-        //0.00199916x^{2}-0.450253x+35.7879
-
-        double finalDistance = calculatedDistance + parabolaError;
-        
-        SmartDashboard.putNumber("initial distance", calculatedDistance);
-        SmartDashboard.putNumber("final distance", finalDistance);
-        SmartDashboard.putNumber("focalLength", focalLength);
-        SmartDashboard.putNumber("pixelWidth", pixelWidth);
-        SmartDashboard.putNumber("parabola Error", parabolaError);
-        SmartDashboard.putNumber("angle", angle);
-
-        //if ()
-        drawRect(pts);
-        findCrosshair(pts);
-        
-        if (crosshair != null)
-            drawCrosshair();
-        }
-        
-        return rects;
-
+            if (hubCrosshair != null)
+                drawCrosshair(hubCrosshair);
+                calculateAngle(hubAngle, hubCrosshair);
+                SmartDashboard.putNumber("Angle", hubAngle);
+               //. SmartDashboard.putNumber("finalAngle", finalAngle);
+            }
     }
 
     public RotatedRect findLargestRect(RotatedRect[] rects) {
@@ -262,10 +356,7 @@ public class VisionProcessorSubsystem extends SubsystemBase {
         for (int i = 0; i < rects.length; i++) {
             if (rects[i].size.area() > rect.size.area())
                 rect = rects[i];
-
         }
-
-
         return rect;
     }
 
@@ -282,31 +373,37 @@ public class VisionProcessorSubsystem extends SubsystemBase {
         int j;
         for (int i = 0; i < 4; i++) {
             j = (i + 1) % 4;
-            if (crosshair == null || (pts[i].y + pts[j].y) / 2 < crosshair.y)
-                crosshair = new Point((pts[i].x + pts[j].x) / 2, (pts[i].y + pts[j].y) / 2);
-
+            if (hubCrosshair == null || (pts[i].y + pts[j].y) / 2 < hubCrosshair.y)
+                hubCrosshair = new Point((pts[i].x + pts[j].x) / 2, (pts[i].y + pts[j].y) / 2);
         }
-
     }
 
     // Draw the crosshair on the frame
-    public void drawCrosshair() {
+    public void drawCrosshair(Point crosshair) {
         Imgproc.line(mat, new Point(crosshair.x - 5, crosshair.y - 5), new Point(crosshair.x + 5, crosshair.y + 5), Constants.BLUE, 3);
         Imgproc.line(mat, new Point(crosshair.x - 5, crosshair.y + 5), new Point(crosshair.x + 5, crosshair.y - 5), Constants.BLUE, 3);
-
     }
 
     // Calculate horizontal turret angle
-    public void calculateAngle() {
+    public void calculateAngle(double angle, Point crosshair) {
         pixelDistance = (int) crosshair.x - Constants.IMG_HOR_MID;
+        SmartDashboard.putNumber("pixelDistance", pixelDistance);
         angle = pixelDistance * Constants.HOR_DEGREES_PER_PIXEL;
-        angleEntry.setDouble(angle);
+        //double a = -0.00549184;
+        //double b = 0.100438;
+        //double c = -1.47937;
 
-    }
+        //double parabolaError = a * Math.pow(angle, 2) + b * angle + c;
+        //-\ 0.00549184x^{2}+0.100438x-1.47937
+        //finalAngle = (int) (angle + parabolaError);
+
+        angleEntry.setDouble(angle);
+    };
+
 
     // Getter for angle
     public double getAngle() { 
-        return angle;
+        return hubAngle;
     }
 
     public Thread getVisionThread() {
