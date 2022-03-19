3be29165d7a7cbae854b9b8d969f554ded879c43
raje2v <rajeevbiz@gmail.com>
2022-03-18 23:15:31 -0400

    Updated Debug Subsystem

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -60,8 +60,8 @@ public class Robot extends TimedRobot {
       visionThread.start();
     }
     else{
-      visionThread = new CameraSubsystem(RobotMap.RingLight).getVisionThread();
-      visionThread.start();
+      // visionThread = new CameraSubsystem(RobotMap.RingLight).getVisionThread();
+      // visionThread.start();
     }
 
     RobotMap.YellowLight.set(true);
